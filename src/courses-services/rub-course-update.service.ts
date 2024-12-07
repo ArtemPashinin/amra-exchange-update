7dfa21644275483f5ad2cb15c2f33cc09f9f352a
artem <artemka77752@gmail.com>
2024-12-07 15:53:01 +0300

    init

--- src/courses-services/rub-course-update.service.ts
+++ src/courses-services/rub-course-update.service.ts
@@ -54,11 +54,11 @@ export class RubUpdateCourseService {
 
   public async updateAedToRub() {
     const [ask, bid] = await this.appService.getOffer('USDT', 'RUB');
-    const bidCourse = (bid.price - bid.price * Fee.CRYPTO) / 3.6;
-    const askCourse = 1 / (ask.price + (ask.price * Fee.CRYPTO) / 3.6);
+    const bidCourse = (bid.price - bid.price * Fee.USDT_RUB) / 3.6;
+    const askCourse = 1 / (ask.price + (ask.price * Fee.USDT_RUB) / 3.6);
     // bidCourse > askCourse
-    await this.currencyExchangeService.updateCourse('AED', 'RUB', askCourse);
-    await this.currencyExchangeService.updateCourse('RUB', 'AED', bidCourse);
+    await this.currencyExchangeService.updateCourse('AED', 'RUB', bidCourse);
+    await this.currencyExchangeService.updateCourse('RUB', 'AED', askCourse);
   }
 
   public getTasks() {
